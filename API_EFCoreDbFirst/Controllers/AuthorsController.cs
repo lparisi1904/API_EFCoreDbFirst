e174e62474576fe2081d93edd64f9f2ab0d79e30
lparisi1904 <lparisi1904@gmail.com>
2023-02-07 10:28:58 +0100

    Test

---
+++ API_EFCoreDbFirst/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API_EFCoreDbFirst.Repository;
+using API_EFCoreDbFirst.Models;
+using API_EFCoreDbFirst.Dto;
+
+namespace API_EFCoreDbFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IDataRepository<Author, AuthorDto> _repository;
+
+        public AuthorsController(IDataRepository<Author, AuthorDto> dataRepository)
+        {
+            _repository = dataRepository;
+        }
+
+        [HttpGet]
+
+
+    }
+}
