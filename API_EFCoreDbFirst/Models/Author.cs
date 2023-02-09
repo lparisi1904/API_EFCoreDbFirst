47fef7f0bce28ec232a92f595c8716c3d78a805d
lparisi1904 <lparisi1904@gmail.com>
2023-02-09 18:16:48 +0100

    Update_20230209

--- API_EFCoreDbFirst/Models/Author.cs
+++ API_EFCoreDbFirst/Models/Author.cs
@@ -6,11 +6,11 @@ namespace API_EFCoreDbFirst.Models;
 
 public partial class Author
 {
-
     public Author()
     {
         BookAuthors = new HashSet<BookAuthors>();
     }
+
     [Key]
     public long Id { get; set; }
 
@@ -20,16 +20,3 @@ public partial class Author
 
     public virtual ICollection<BookAuthors> BookAuthors { get; set; } 
 }
-
-
-//public Author()
-//{
-//    BookAuthors = new HashSet<BookAuthors>();
-//}
-
-//public long Id { get; set; }
-//public string Name { get; set; }
-
-//public virtual AuthorContact AuthorContact { get; set; }
-//public virtual ICollection<BookAuthors> BookAuthors { get; set; }
-//    }
